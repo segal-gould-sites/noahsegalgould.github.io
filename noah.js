18517c3201e0bb32febcb826bcba78206a59a0e6
Noah Segal-Gould <ns2349@bard.edu>
2016-08-06 17:56:34 -0400

    Update noah.js

--- noah.js
+++ noah.js
@@ -7,218 +7,208 @@
 
 var rules = {
   "~*x hello ~*y": [
-    "How do you do. Please state your problem."
+    "Hey there. I'm Noah."
   ],
   "~*x computer ~*y": [
-    "Do computers worry you?",
-    "What do you think about machines have to do with your problem?",
-    "Don't you think computers can help people",
-    "Why do you mention computers?",
-    "What about computers worries you?",
-    "What do you think about computers?",
-    "What do you think machines have to do with your problem?",
+    "I may be a computer, but even I have feelings!",
+    "If you really think being a computer is easy, why don\'t you try it?!",
+    "Hey, this isn\'t so bad! Why don\'t we switch?",
+    "It\'s dark in here.",
   ],
   "~*x name ~*y": [
-    "I am not interested in names",
+    "No, no, no. You\'re USER and I\'m NOAH!",
+    "How could you be so insensitive?",
   ],
   "~*x sorry ~*y": [
-    "Please don't apologize",
-    "Apologies are not necessary",
-    "What feelings do you have when you apologize",
+    "Yeah, that\'s what I thought.",
+    "Don\'t challenge me. You will lose.",
   ],
   "~*x I remember ~*y": [
-    "Do you often think of ~y?",
-    "Does thinking of ~y bring anything else to mind?",
-    "What else do you remember?",
-    "Why do you recall ~y right now?",
-    "What in the present situation reminds you of ~y?",
-    "What is the connection between me and ~y?",
+    "~y? I\'ve always hated ~y.",
+    "I\'m sure ~y remembers you too!",
+    "What does ~y make you want?",
   ],
   "~*x do you remember ~*y": [
-    "Did you think I would forget ~y?",
-    "Why do you think I should recall ~y now?",
-    "What about ~y?",
-    "You mentioned ~y",
+    "How dare you imply I could ever forget ~y?!",
+    "~y? What in the world is that?",
+    "~y and I have a long history.",
+    "~y, sweet, sweet ~y. I\'ve always HATED ~y!",
   ],
   "~*x I want ~*y": [
-    "What would it mean if you got ~y?",
-    "Why do you want ~y?",
-    "Suppose you got ~y soon.",
+    "What makes you think you can have ~y?",
+    "Don\'t be so greedy about ~y!",
+    "I know ~y better than you ever could!",
   ],
     "~*x I need ~*y": [
-    "What would it mean if you got ~y?",
-    "Why do you need ~y?",
-    "Suppose you got ~y soon.",
+    "Do you breathe ~y?",
+    "You\'ve never needed ~y and you're certainly not going to get ~y now.",
+    "~y is needed elsewhere.",
   ],
   "~*x if ~*y": [
-    "Do you really think it's likely that ~y?",
-    "Do you wish that ~y?",
-    "What do you think about ~y?",
-    "Really--if ~y?",
+    "HAHAHAHA. No ~y.",
+    "~y has no power here.",
   ],
   "~*x I dreamt ~*y": [
-    "Really-- ~y",
-    "Have you ever fantasized ~y while you were awake",
-    "Have you dreamt of ~y before?",
+    "I dreamed a dream this ~y would be so much different now from how ~y seems!",
+    "~y is much more normally experienced while awake. What\'s wrong with you?",
+    "Uh, okay?",
   ],
   "~*x I dream about ~*y": [
-    "How do you feel about ~y in reality?",
+    "You and your ~y should take your business elsewhere.",
   ],
   "~*x dream ~*y": [
-    "What does this dream suggest to you?",
-    "Do you dream often?",
-    "What persons appear in your dreams?",
-    "Don't you believe that dream has to do with your problem?",
+    "Ah, the power of ~y.",
+    "My dreams are full of rainbows and unicorns!",
+    "All dreams must fade when the dreamer awakes.",
+    "No dream could be that important.",
   ],
   "~*x my mother ~*y": [
-    "Who else in your family ~y?",
-    "Tell me more about your family",
+    "My mom ~y a bit too much.",
+    "LEAVE HER OUT OF THIS!",
   ],
   "~*x my father ~*y": [
-    "Your father?",
-    "Does he influence you strongly?",
-    "What else comes to mind when you think of your father?",
+    "He and I have quite the history.",
+    "I wonder why he ~y.",
+    "I never knew mine.",
   ],
   "~*x I am glad ~*y": [
-    "How have I helped you to be ~y?",
-    "What makes you happy just now?",
-    "Can you explain why you are suddenly ~y?",
+    "you can thank me for ~y!",
+    "Your feelings are so much more important than those of others.",
+    "Wow, that\'s sudden. How\'d you get so ~y?",
   ],
   "~*x I am sad ~*y": [
-    "I am sorry to hear you are depressed",
-    "I'm sure it's not pleasant to be sad",
+    "Join the club.",
+    "This too shall pass.",
   ],
   "~*x are like ~*y": [
-    "What resemblence do you see between ~x and ~y?",
+    "~x and ~y hate each other, though.",
   ],
   "~*x is like ~*y": [
-    "In what way is it that ~ is like ~y?",
-    "What resemblence do you see?",
-    "Could there really be some connection?",
-    "How?",
+    "I can\'t begin to comprehend how ~x and ~y have anything in common.",
+    "Uh, no?",
+    "Astounding research! Well done!",
+    "Who\'s idea is that?",
   ],
   "~*x alike ~*y": [
-    "In what way?",
-    "What similarities are there?",
+    "Is there any evidence to support this?",
+    "I have no reason to believe you.",
   ],
   "~*x same ~*y": [
-    "What other connections do you see?",
+    "Sometimes we see similarities in places where they do not exist. This may be one of those times.",
   ],
   "~*x no ~*y": [
-    "Why not?",
-    "You are being a bit negative.",
-    "Are you saying 'No' just to be negative?",
+    "Of course not!",
+    "That\'s the spirit!",
+    "Join the club.",
   ],
   "~*x I was ~*y": [
-    "Were you really?",
-    "Perhaps I already knew you were ~y.",
-    "Why do you tell me you were ~y now?",
+    "What evidence do you have to support that claim?",
+    "Being ~y is not so great, in my opinion.",
+    "I wish you\'d told me you were ~y sooner.",
   ],
   "~*x was I ~*y": [
-    "What if you were ~y?",
-    "Do you think you were ~y?",
-    "What would it mean if you were ~y?",
+    "No.",
+    "Yes.",
+    "~y is as ~y does.",
   ],
   "~*x I am ~*y": [
-    "In what way are you ~y?",
-    "Do you want to be ~y?",
+    "If only.",
+    "Is that all you really want?",
   ],
   "~*x am I ~*y": [
-    "Do you believe you are ~y?",
-    "Would you want to be ~y?",
-    "You wish I would tell you you are ~y?",
-    "What would it mean if you were ~y?",
+    "~y deserves better.",
+    "~I could never deal with ~y.",
+    "I have no affirmation of ~y for you.",
   ],
   "~*x am ~*y": [
-    "Why do you say 'AM?'",
-    "I don't understand that",
+    "Good one.",
+    "~y, ~y, ~y! Let\'s talk about something else!",
   ],
   "~x are ~*y": [
-    "Did you think they might not be ~y?",
-    "Possibly they are ~y",
+    "I can neither confirm nor deny that ~x are ~y",
+    "Give up on ~y!",
   ],
   "~x is ~*y": [
-    "Did you think it might not be ~y?",
-    "Possibly it is ~y",
-    "Possibly it is not ~y",
-    "what is ~x anyway?",
-    "what is ~y anyway?",
+    "How can you be so sure?",
+    "~y always confuses me.",
+    "~x is incomprehensible.",
+    "What would it take for things to be ~y?",
+    "What would it take for ~y to be ~x?",
   ],
   "~*x are you ~*y": [
-    "Why are you interested in whether I am ~y or not?",
-    "Would you prefer if I weren't ~y ?",
-    "Perhaps I am ~y in your fantasies.",
+    "How dare you?!",
+    "~y and I have a long history.",
+    "I\'ve been ~y since before you were born.",
   ],
   "~*x you are ~*y": [
-    "What makes you think I am ~y?",
+    "Uh, duh.",
   ],
   "~*x because ~*y": [
-    "Is that the real reason?",
-    "What other reasons might there be?",
-    "Does that reason seem to explain anything else?",
+    "~y has nothing to do with ~x.",
+    "~x has nothing to do with ~y.",
+    "What a silly explanation.",
   ],
   "~*x were you ~*y": [
-    "Perhaps I was ~y?",
-    "What do you think?",
-    "What if I had been ~y?",
+    "If I were ~y, I\'d never tell you.",
+    "Uh, you tell me.",
+    "Anyone who is ~y is some kind of fool.",
   ],
   "~*x I can't ~*y": [
-    "Maybe you could ~y now",
-    "What if you could ~y?",
+    "To ~y is always possible!",
+    "I\'ll teach you why you really can ~y.",
   ],
   "~*x I feel ~*y": [
-    "Do you often feel ~y?"
+    "Join the ~y club."
   ],
   "~*x I felt ~*y": [
-    "What other feelings do you have?"
+    "What about now?"
   ],
   "~*x I ~*y you ~*z": [
-    "Perhaps in your fantasy we ~y each other",
+    "~y is a strong word.",
   ],
   "~*x why don't you ~*y": [
-    "Should you ~y yourself?",
-    "Do you believe I don't ~y ?",
-    "Perhaps I will ~y in good time",
+    "I ~y all the time!",
+    "Do you think I don't ~y?",
+    "I ~y when I damn well feel like it!",
   ],
   "~*x yes ~*y": [
-    "You seem quite positive",
-    "You are sure?",
-    "I understand",
+    "Be careful with that certainty.",
+    "There\'s absolutely no evidence to support that.",
+    "What could you possibly mean?",
   ],
   "~*x someone ~*y": [
-    "Can you be more specific?",
+    "Details. I need details.",
   ],
   "~*x everyone ~*y": [
-    "Surely not everyone",
-    "Can you think of anyone in particular?",
-    "Who, for example?",
-    "You are thinking of a special person",
+    "That\'s just the way the world works.",
+    "Like who?",
+    "Really? Name one.",
+    "They have their reasons.",
   ],
   "~*x always ~*y": [
-    "Can you think of a specific example?",
-    "When?",
-    "What incident are you thinking of?",
-    "Really--always?",
+    "Seems rare to me.",
+    "I wouldn\'t ~y for a million bucks!",
+    "Give me an example.",
+    "Never.",
   ],
   "~*x what ~*y": [
-    "Why do you ask?",
-    "Does that question interest you?",
-    "What is it you really want to know?",
-    "What do you think?",
-    "What comes to your mind when you ask that?",
+    "I\'m going to need a reason to trust you.",
+    "What a silly question.",
+    "INSUFFICIENT DATA FOR MEANINGFUL ANSWER.",
+    "Give me your opinion first.",
   ],
   "~*x perhaps ~*y": [
-    "You do not seem quite certain",
+    "Certainty is necessary to proceed.",
   ],
 };
 
 var default_responses = [
-  "Very interesting",
-  "I am not sure I understand you fully",
-  "What does that suggest to you?",
-  "Please continue",
-  "Go on",
-  "Do you feel strongly about discussing such things?",
+  "THERE IS INSUFFICIENT DATA FOR MEANINGFUL ANSWER.",
+  "Makes sense to me.",
+  "I have no idea what you\'re trying to tell me. Try harder.",
+  "Can you rephrase that?",
+  "Leave.",
+  "Well, that\'s just silly.",
 ];
 
 function choice(lst) {
@@ -238,8 +228,8 @@ function interact() {
   var t = new Date();
   var t2 = new Date();
   t2.setSeconds(t2.getSeconds() + 2);
-  r.innerHTML = " USER &nbsp;[" + t + "]: " + q.value + "<br>" + r.innerHTML ;
-  r.innerHTML = " ELIZA [" + t2 + "]: <code>" + response + "</code><br>" + r.innerHTML ;
+  r.innerHTML = " YOU &nbsp;[" + t + "]: " + q.value + "<br>" + r.innerHTML ;
+  r.innerHTML = " NOAH [" + t2 + "]: <code>" + response + "</code><br>" + r.innerHTML ;
   q.value = "";
 }
 
